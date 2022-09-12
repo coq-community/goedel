969c54c5bbcd0b569bb8a2c5231848b856b109bd
Pierre Casteran <pierre.casteran@labri.fr>
2022-09-12 16:35:35 +0200

    make explicit Requirements to Pocklington

---
+++ theories/tryCoqPrime.v
@@ -0,0 +1,14 @@
+From Coqprime Require Import NatAux.
+
+Goal divide 3 6. 
+Proof. now exists 2. Qed.
+
+Goal ~ divide 0 1.
+Proof. intros [q Hq]. discriminate.  Qed.
+
+From Coqprime Require Import ZCAux. 
+
+Goal (3 | 6).
+Proof. now exists 2. Qed.
+
+Search Zis_gcd. 
